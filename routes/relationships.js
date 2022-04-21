755dd7ebb5260bd3b728c4c9a294bd331e29d868
Dani <danicodes94@gmail.com>
2022-04-21 19:07:16 -0400

    Created several endpoints for relationships
    /mentors, /mentees -- facilitating accessing the user's list of mentor relationships and mentee relationships respectively
    /:status -- get relationships by status
    / -- get all relationships, post a new relationship

--- routes/relationships.js
+++ routes/relationships.js
@@ -6,12 +6,12 @@ const relationships = data.relationships;
 const users = data.users;
 
 // require users
-const status = require('../enums/status');
 
 async function validateCurrentSession(req, res) {
     //TODO: check that user is logged in here, none of these routes is accessible without being logged in
 }
-
+// Returns list of relations object
+// Sort by alphabetical order
 router.route('*').get(async(req,res) => {
     try {
         await validateCurrentSession(req, res);
@@ -22,9 +22,7 @@ router.route('*').get(async(req,res) => {
     }
 });
 
-router.route('*') 
 
-// domain/relationships
 router.route('/')
 .get(async(req, res) =>{
     // Show all the relationships in the database
@@ -42,6 +40,30 @@ router.route('/')
         res.render('frames/relationships', {layout: 'profile', relationships: returnList});
     }
     catch(e) {
+        res.status(500).render('frames/error', {layout: 'profile', error: "Internal server error"});
+    }
+})
+.post(async(req, res) => {
+    // Create a new relationship
+    let menteeId, mentorId;
+    try {
+        validate.checkArgLength(req.body, 4);
+        validate.checkIsEmptyString(req.body.relationshipDescription);
+        mentorId = validate.convertID(req.body.mentorId); // TODO: decide if this should be a string or object id
+        menteeId = validate.convertID(req.body.menteeId);
+        if (!(req.body.relationshipCategory instanceof data.categories)) throw `Error`;
+    }
+    catch(e) {
+        res.status(400).render('frames/error', {layout: 'profile', error: "Internal server error"});
+        return;
+    }
+
+    let relationshipObj;
+    try {
+        relationshipObj = await relationships.createRelationship(req.body.relationshipDescription, mentorId, menteeId, req.body.relationshipCategory);
+        res.render('frame/request', {relationship: relationshipObj, created: true});
+    }
+    catch(e){
         res.status(500).render('frames/error', {layout: 'profile', error: "Internal server error"});
     }
 });
@@ -62,12 +84,51 @@ router.route('/:status')
         // TODO:if in mentee view, show all your mentors
         let mentorList = await users.getMentorList();
         returnList = await relationships.filterRelationshipsByStatus(mentorList, req.params.status);
+        
+        let returnObjects = returnList.map(async(returnId) => {
+            return await relationships.getRelationshipById(returnId);
+        });
     
-        //Decide what exactly from the relationship needs to be returned.
-        // username of connection and url?
-        res.render('frames/relationships', {layout: 'profile', relationships: returnList});
+        //return relationship objects
+        res.render('frames/relationships', {layout: 'profile', relationships: returnObjects});
     }
     catch(e) {
         res.status(500).render('frames/error', {layout: 'profile', error: "Internal server error"});
     }
-})
+});
+
+router.route('/mentors')
+.get(async(req, res) => {
+    let returnList;
+    try {
+        let mentorRelationships = await users.getMentorList();
+        let relationshipObjects = mentorRelationships.map(async(mentorRelationshipId) => {
+            return await relationships.getRelationshipById(mentorRelationshipId);
+        });
+        //return relationship objects
+        res.render('frames/relationships', {layout: 'profile', relationships: relationshipObjects});
+    }
+    catch(e) {
+        res.status(500).render('frames/error', {layout: 'profile', error: "Internal server error"});
+    }
+    
+});
+
+router.route('/mentees')
+.get(async(req, res) => {
+    let returnList;
+    try {
+        let menteeRelationships = await users.getMenteeList();
+        let relationshipObjects = menteeRelationships.map(async(menteeRelationshipId) => {
+            return await relationships.getRelationshipById(menteeRelationshipId);
+        });
+        //return relationship objects
+        res.render('frames/relationships', {layout: 'profile', relationships: relationshipObjects});
+    }
+    catch(e) {
+        res.status(500).render('frames/error', {layout: 'profile', error: "Internal server error"});
+    }
+    
+});
+
+module.exports = router;
