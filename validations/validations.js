7cf0beeddc45efb7388cb520080fa471fccc0699
brndnm <bmurray@stevens.edu>
2022-05-03 19:49:31 -0400

    Forgot to commit validiations.js

--- validations/validations.js
+++ validations/validations.js
@@ -83,40 +83,6 @@ let validateDate = function(date, varName) {
     return parsedDate;
 }
 
-const checks = function checks(name, bio, age, searchTags, mentorRelations, menteeRelations, myPosts){
-    if (!(typeof name == 'string') || (name == '')){
-        throw new Error("name needs to be a non-zero string");
-    }
-    if (!(typeof bio == 'string')){
-        throw new Error("bio needs to be a string");
-    }
-    if (!(typeof age == 'number') || (age > 100) || (age < 0)){
-        throw new Error("age must be a number between 0 and 100");
-    }
-    if (!Array.isArray(searchTags)){
-        throw new Error("searchTags must be an array of strings");
-    }
-    let flag = false;
-    for (let i = 0; i < searchTags.length; i++){
-        if (typeof searchTags[i] != 'string'){
-            flag = true;
-        }
-    }
-    if (flag){
-        throw new Error("searchTags must contain strings");
-    }
-    console.log(mentorRelations);
-    if (!Array.isArray(mentorRelations)){
-        throw new Error("mentorRelations must be an array");
-    }
-    if (!Array.isArray(menteeRelations)){
-        throw new Error("menteeRelations must be an array");
-    }
-    if (!Array.isArray(myPosts)){
-        throw new Error("myPosts must be an array");
-    }
-}
-
 module.exports = {
     validateId      : validateId,
     validateString  : validateString,
