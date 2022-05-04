cb48b5c4e012bafacd2731a9545eff5ff39a66c7
SaiHarishVitta <svitta@stevens.edu>
2022-05-03 21:54:39 -0400

    Error Check Update

--- data/users.js
+++ data/users.js
@@ -7,15 +7,18 @@ const {ObjectId} = require("mongodb");
 const validate = require('../validations/data');
 
 async function getPersonById(id){
+    validations.validateId(id);
     // In this function we need to find the user for a given id and return them.
     const userCollection = await users();
     const person = await userCollection.findOne({ _id: ObjectId(id) });
     // Return the whole person
     console.log(person);
+    person._id = person._id.toString();
     return person;
 }
 
 async function updateUser(id, name, bio, age, searchTags, mentorRelations, menteeRelations, myPosts){
+    validations.validateId(id);
     validate.checks(name, bio, age, searchTags, mentorRelations, menteeRelations, myPosts);
     // I need to get the password/username for the given id to put in updateduser here
     const updateduser = {
@@ -31,28 +34,32 @@ async function updateUser(id, name, bio, age, searchTags, mentorRelations, mente
     const updated = await userCollection.updateOne(
         { _id : ObjectId(id) },
         {$set : updateduser}
-    );
+        );
     if (updated.modifiedCount == 0){
         throw "Error: nothing to be updated."
     }
-    const ret = await userCollection.findOne({_id : ObjectId(id)});
+    let ret = await userCollection.findOne({_id : ObjectId(id)});
+    ret._id = ret._id.toString();
     return ret;
 }
-
+    
 async function updatePassword(id, password){
+    validations.validateId(id);
+    password = validations.validateString(password);
     let hashedpassword = await bcrypt.hash(password, 8);
     const userCollection = await users();
     const updated = await userCollection.updateOne(
         { _id : ObjectId(id)},
         {$set: {password : hashedpassword} }
-    );
+        );
     if (updated.modifiedCount == 0){
         throw "Error: could not update password."
     }
     const ret = await userCollection.findOne({_id : ObjectId(id)});
+    ret._id = ret._id.toString();
     return ret;
 }
-
+        
 
 
 /**** Functions for testing relationship routes ****/
@@ -116,7 +123,7 @@ async function getUserRelationships(userId){
  */
 async function updateUserRelationships(userId, relationshipObj){
     validate.checkArgLength(arguments, 2);
-    userId = validate.convertID(userId);
+    userId = validate.convertID();
 
     let mentorId = validate.convertID(relationshipObj.mentor);
     let menteeId = validate.convertID(relationshipObj.mentee);
@@ -170,8 +177,8 @@ async function updateUserRelationships(userId, relationshipObj){
  *          or throw an error in case if user is not found
  */
 let addToMyPosts = async function(userId, postId) {
-    userId = validations.validateId(userId);
-    postId = validations.validateId(postId);
+    userId = validations.validateId(userId, "User ID");
+    postId = validations.validateId(postId, "Post ID");
 
     let usersCollection = await getUsersCollection();
     let {value: updatedUser} = await usersCollection.findOneAndUpdate(
@@ -203,7 +210,7 @@ let getMyPosts = async function(userId) {
  *          - False in case of invalid user
  */
 let isValidUser = async function(userId) {
-    userId = validations.validateId(userId);
+    userId = validations.validateId(userId, "User ID");
 
     let usersCollection = await getUsersCollection();
     let user = usersCollection.findOne({_id: ObjectId(userId)});
--- routes/users.js
+++ routes/users.js
@@ -7,67 +7,68 @@ const validate = require('../validations/data');
 
 router.get('/:userid', async (req, res) => {
     // Show profile information
+    let userId = req.params['userid'];
     try {
-        if (validations.validateId(req.params['userid']) == req.params['userid']){
-            const id = req.params['userid'];
-            const ret = await userData.getPersonById(id);
-            console.log(ret);
-        }
-        else{
-            res.status(400).send();
-        }
+        userId = validations.validateId(userId);
+    } catch (e) {
+        return res.status(400).json("Error: " + e);
+    }
+    try {
+        const ret = await userData.getPersonById(userId);
+        return res.status(200).json(ret);
     }
     catch(e){
         console.log(e);
-        res.status(500).send();
+        res.status(500).json(e);
     }
 });
 
 router.put('/:userid', async (req, res) => {
     // Update user in database
+    let userId = req.params['userid'];
+    const id = req.params['userid'];
+    const name = req.body['name'];
+    const bio = req.body['bio'];
+    const age = req.body['age'];
+    const searchTags = req.body['searchTags'];
+    const mentorRelations = req.body['mentorRelationships'];
+    const menteeRelations = req.body['menteeRelationships'];
+    const myPosts = req.body['myPosts'];
     try {
-        if (validations.validateId(req.params['userid']) == req.params['userid']){
-            const id = req.params['userid'];
-            const name = req.body['name'];
-            const bio = req.body['bio'];
-            const age = req.body['age'];
-            const searchTags = req.body['searchTags'];
-            const mentorRelations = req.body['mentorRelationships'];
-            const menteeRelations = req.body['menteeRelationships'];
-            const myPosts = req.body['myPosts'];
-            validate.checks(name, bio, age, searchTags, mentorRelations, menteeRelations, myPosts);
-            // Get all the information out of the id to pass to updateUser
-            const ret = await userData.updateUser(id, name, bio, age, searchTags, mentorRelations, menteeRelations, myPosts);
-            // Return the updated collection? (To html page?)
-            console.log(ret);
-        }
-        else{
-            res.status(400).send();
-        }
+        userId = validations.validateId(userId);
+        validate.checks(name, bio, age, searchTags, mentorRelations, menteeRelations, myPosts);
+    } catch (e) {
+        return res.status(400).json("Error: " + e);
+    }
+
+    try {
+        const ret = await userData.updateUser(userId, name, bio, age, searchTags, mentorRelations, menteeRelations, myPosts);
+        return res.status(200).json(ret);
     }
     catch(e){
         console.log(e);
-        res.status(500).send();
+        res.status(500).json(e);
     }
 });
 
 router.put('/:userid/reset', async (req, res) => {
     // Update user in database
+    let userId = req.params['userid'];
+    let password = req.body['password'];
     try {
-        if (validations.validateId(req.params['userid']) == req.params['userid']){
-            const id = req.params['userid'];
-            const password = req.body['password'];
-            const ret = await userData.updatePassword(id, password);
-            console.log("Password updated successfully");
-            console.log(ret);
-        }
-        else{
-            res.status(400).send();
-        }
+        userId = validations.validateId(userId);
+        password = validations.validateString(password);
+    } catch (e) {
+        return res.status(400).json("Error: " + e);
+    }
+
+    try {
+        const ret = await userData.updatePassword(userId, password);
+        return res.status(200).json(ret);
     }
     catch(e){
         console.log(e);
-        res.status(500).send();
+        res.status(500).json(e);
     }
 });
 
