18d2984bd92805891e326ca098df9ee65870fb8b
Ethan Grzeda <32401312+egrzeda@users.noreply.github.com>
2022-05-06 22:30:54 -0400

    Front end login about (#14)
    * login and signup feature
    
    created changes to data, routes, and addded pages with handlebars.
    
    landing page is still pending which will be replaced by the welcome
    page, css and html will be updated.
    
    * constructRoutes - reverted name
    
    * Updated Signup and LoginPage
    
    Commit Includes:
      Front-end is functional
      Backend is functional
      validations in backend are done
    Need to do:
      Front-end validations
      Authentication middleware remaining
    
    * 2ndcommit
    
    * updated validations and routes
    
    everything is tested
    
    * Created a new login client-side script and implemented error checks for username and password fields. Next step is programming the case for if the user inputs both fields correctly
    
    * Improved client-side validation for login fields by adding checks for length and customizing error text a little more
    
    * Fixed spaces checks for username and password inputs so that it shows error(s) if there are any spaces in the input, not just if the entire input is only spaces. Next step is adding checks to make sure that the username and password inputs consist of only alphanumeric characters.
    
    * Finished login page validation. Next step is writing the signup page validation script
    
    * Finished signup form client-side validation script.
    
    * Tweaked and removed a few comments. Ready for merge with dev.
    
    Co-authored-by: Yash Kosambia <90350327+yashkosambia14@users.noreply.github.com>
    Co-authored-by: Danielle Williams <danicodes94@gmail.com>

--- data/index.js
+++ data/index.js
@@ -1,13 +1,14 @@
-const relationships = require('./relationships');
-const postsData = require('./posts');
-const users = require('./users');
-const chat = require('./chat');
+const relationships = require("./relationships");
+const postsData = require("./posts");
+const users = require("./users");
+const chat = require("./chat");
 const login_users = require("./loginUsers");
 
 module.exports = {
-    users : users,
-    relationships: relationships,
-    posts: postsData,
-    chat: chat,
-    login_users: login_users,
+  users: users,
+  relationships: relationships,
+  posts: postsData,
+  users: users,
+  chat: chat,
+  login_users: login_users,
 };
